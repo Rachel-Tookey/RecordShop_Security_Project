3dc7086bcc3448fa6f5236bc97b5405e97c6a23f
Fabiola Palmas <fabiola.palmas@gmail.com>
2024-07-22 13:39:25 +0100

    Basic queries methods in recordService tests written and passed.

--- src/main/java/com/example/group/project/service/impl/RecordServiceImpl.java
+++ src/main/java/com/example/group/project/service/impl/RecordServiceImpl.java
@@ -6,11 +6,13 @@ import com.example.group.project.service.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class RecordServiceImpl implements RecordService {
-
+    @Autowired
     private final RecordRepository recordRepository;
 
     @Autowired
@@ -22,8 +24,12 @@ public class RecordServiceImpl implements RecordService {
         return recordRepository.findAll();
     }
 
-    public List<Record> getRecordsByName(String recordName) {
+    public List<Record> getRecordByName(String recordName) {
         return recordRepository.findByNameIgnoreCase(recordName);
+    }
+
+    public List<Record> getRecordsByArtist(String artist) {
+        return recordRepository.findByArtistIgnoreCase(artist);
     }
 
     public List<Record> getRecordsByNameAndArtist(String artist, String recordName) {
--- src/test/java/com/example/group/project/serviceTests/RecordServiceImplTests.java
+++ src/test/java/com/example/group/project/serviceTests/RecordServiceImplTests.java
@@ -4,6 +4,7 @@ package com.example.group.project.serviceTests;
 import com.example.group.project.model.entity.Record;
 import com.example.group.project.model.repository.RecordRepository;
 import com.example.group.project.service.impl.RecordServiceImpl;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -42,7 +45,6 @@ public class RecordServiceImplTests {
 
     @Test
     public void getAllRecords_withNoRecordsInDatabase_returnsEmptyList() {
-
         List<Record> noRecords = List.of();
 
         Mockito
@@ -53,5 +55,100 @@ public class RecordServiceImplTests {
 
     }
 
+    @Test
+    public void getRecord_givenExistingArtist_returnsArtistRecords() {
+        Record record1 = new Record();
+        Record record2 = new Record();
+
+        String artist = "Michael Jackson";
+
+        List<Record> recordsByArtist = List.of(record1, record2);
+
+        Mockito
+                .when(recordRepository.findByArtistIgnoreCase(artist))
+                .thenReturn(recordsByArtist);
+
+        Assertions.assertEquals(recordsByArtist.size(), recordServiceImpl.getRecordsByArtist(artist).size());
+        Assertions.assertEquals(recordsByArtist, recordServiceImpl.getRecordsByArtist(artist));
+    }
+
+    @Test
+    public void getRecordByArtist_givenNotExistingArtist_returnsEmptyList() {
+        String artist = "Bob Dylan";
+
+        List<Record> noRecordsFound = List.of();
+
+        Mockito
+                .when(recordRepository.findByArtistIgnoreCase(artist))
+                .thenReturn(noRecordsFound);
+
+        Assertions.assertTrue(recordServiceImpl.getRecordsByArtist(artist).isEmpty());
+
+    }
+
+    @Test
+    public void getRecordByName_givenExistingRecord_returnsThatRecord() {
+        String recordName = "Rec1";
+        Record record1 = new Record();
+        record1.setName(recordName);
+
+        List<Record> recordByName = List.of(record1);
+
+        Mockito
+                .when(recordRepository.findByNameIgnoreCase(recordName))
+                .thenReturn(recordByName);
+
+        Assertions.assertEquals(recordByName.size(), recordServiceImpl.getRecordByName(recordName).size());
+        Assertions.assertEquals(recordByName, recordServiceImpl.getRecordByName(recordName));
+    }
+
+    @Test
+    public void getRecordByName_givenNotExistingRecord_returnsEmptyList() {
+        String recordName = "Rec1";
+
+        List<Record> recordByName = List.of();
+
+        Mockito
+                .when(recordRepository.findByNameIgnoreCase(recordName))
+                .thenReturn(recordByName);
+
+        Assertions.assertTrue(recordServiceImpl.getRecordByName(recordName).isEmpty());
+    }
+
+    @Test
+    public void getRecordsByNameAndArtist_givenExistingArtistAndName_returnsCorrectRecords () {
+        String recordName = "Thriller";
+        String artist = "Michael Jackson";
+
+        Record record1 = new Record();
+        record1.setName(recordName);
+        record1.setArtist(artist);
+
+        List<Record> recordByNameAndArtist = List.of(record1);
+
+        Mockito
+                .when(recordRepository.findByNameAndArtistIgnoreCase(recordName, artist))
+                .thenReturn(recordByNameAndArtist);
+
+        Assertions.assertEquals(recordByNameAndArtist.size(), recordServiceImpl.getRecordsByNameAndArtist(artist,
+                recordName).size());
+        Assertions.assertEquals(recordByNameAndArtist, recordServiceImpl.getRecordsByNameAndArtist(artist, recordName));
+    }
+
+    @Test
+    public void getRecordsByNameAndArtist_givenNotExistingArtistAndName_returnsEmptyList () {
+        String recordName = "Thriller";
+        String artist = "Michael Jackson";
+
+        List<Record> recordByNameAndArtist = List.of();
+
+        Mockito
+                .when(recordRepository.findByNameAndArtistIgnoreCase(recordName, artist))
+                .thenReturn(recordByNameAndArtist);
+
+        Assertions.assertTrue(recordServiceImpl.getRecordsByNameAndArtist(artist,
+                recordName).isEmpty());
+
+    }
 
 }
