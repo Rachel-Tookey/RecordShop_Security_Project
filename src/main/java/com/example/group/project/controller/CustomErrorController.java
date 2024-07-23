b886629aaf476fd847f5f2a69f84f3d9d49c5bc8
Tookey <rachel.tookey@gmail.com>
2024-07-23 10:46:21 +0100

    Updated custom error controller

--- src/main/java/com/example/group/project/controller/CustomErrorController.java
+++ src/main/java/com/example/group/project/controller/CustomErrorController.java
@@ -1,24 +1,32 @@
 package com.example.group.project.controller;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Slf4j
-@RestController
+@RestControllerAdvice
 public class CustomErrorController implements ErrorController {
 
-    @RequestMapping("/error")
-    public ResponseEntity<?> errorPage(HttpServletRequest request){
-        String url = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
-        log.error("User sent request to " + url + " and an error message has been thrown");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error has occurred. Please try again later");
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> errorPage(WebRequest request, Exception ex){
+
+        Map<String, Object> body = new HashMap<>();
+        String url = ((ServletWebRequest)request).getRequest().getRequestURI().toString();
+        Object details = ex.getMessage();
+        body.put("Further details", details);
+        body.put("You attempted to access the following URL", url);
+
+        log.error("User sent request to " + url + " and the following error occured: " + details);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
 }
