a673a2bad59559afed0d0c6f0fee48f0481f6bd1
Tookey <rachel.tookey@gmail.com>
2024-12-17 23:05:36 +0000

    Fix: ensured the custom error controller sends back correct Http status codes

--- src/main/java/com/example/group/project/controller/CustomErrorController.java
+++ src/main/java/com/example/group/project/controller/CustomErrorController.java
@@ -1,33 +1,57 @@
-//package com.example.group.project.controller;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.boot.web.servlet.error.ErrorAttributes;
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.context.request.ServletWebRequest;
-//import org.springframework.web.context.request.WebRequest;
-//
-//import java.util.LinkedHashMap;
-//import java.util.Map;
-//
-//
-//@Slf4j
-//@RestControllerAdvice
-//public class CustomErrorController implements ErrorController {
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<?> errorPage(WebRequest request, Exception ex){
-//
-//        Map<String, Object> body = new LinkedHashMap<>();
-//        String url = ((ServletWebRequest)request).getRequest().getRequestURI().toString();
-//        Object details = ex.getMessage();
-//        body.put("You attempted to access the following URL", url);
-//        body.put("Further details", details);
-//
-//        log.error("User sent request to " + url + " and the following error occured: " + details);
-//
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
-//    }
-//}
+package com.example.group.project.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+@Slf4j
+@RestControllerAdvice
+public class CustomErrorController implements ErrorController {
+
+    public static HttpStatus getStatusFromCode(int code) {
+        for (HttpStatus status : HttpStatus.values()) {
+            if (status.value() == code) {
+                return status;
+            }
+        }
+        return HttpStatus.NOT_FOUND;
+    }
+
+    @RequestMapping("/error")
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> errorPage(HttpServletRequest request, Exception ex){
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Integer statusCode = null;
+
+        if (status != null) {
+            statusCode = Integer.valueOf(status.toString());
+        }
+
+        HttpStatus castStatus = getStatusFromCode(statusCode);
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        String url = ((ServletWebRequest)request).getRequest().getRequestURI().toString();
+        Object details = ex.getMessage();
+        body.put("You attempted to access the following URL", url);
+        body.put("Further details", details);
+
+        log.error("User sent request to " + url + " and the following error occured: " + details);
+
+        return ResponseEntity.status(castStatus).body(body);
+
+    }
+
+
+}
---
+++ src/main/java/com/example/group/project/dto/PurchaseDTO.java
@@ -0,0 +1,11 @@
+package com.example.group.project.dto;
+
+public class PurchaseDTO {
+
+    public String customer;
+
+    public int id;
+
+    public String discount;
+
+}
