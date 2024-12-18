af0f0653cde2c6f4eb99b40db52c05d2db65cbb8
Tookey <rachel.tookey@gmail.com>
2024-12-18 22:46:06 +0000

    Feat: tidied LoginRequestDTO

--- src/main/java/com/example/group/project/dto/LoginRequestDTO.java
+++ src/main/java/com/example/group/project/dto/LoginRequestDTO.java
@@ -1,17 +1,12 @@
 package com.example.group.project.dto;
 
 
+import lombok.Getter;
+
+@Getter
 public class LoginRequestDTO {
 
     private String username;
     private String password;
-
-    public String getUsername(){
-        return username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
 
 }
