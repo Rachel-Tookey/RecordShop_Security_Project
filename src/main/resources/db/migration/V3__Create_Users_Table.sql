9943dee45cc2547c1ba50abd509c273b4e13af27
Tookey <rachel.tookey@gmail.com>
2024-12-18 16:51:12 +0000

    Feat: added in one user to the database with Admin credentials

--- src/main/resources/db/migration/V3__Create_Users_Table.sql
+++ src/main/resources/db/migration/V3__Create_Users_Table.sql
@@ -1,7 +1,12 @@
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(55) NOT NULL,
+    lastname VARCHAR(55) NOT NULL,
     username VARCHAR(55) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL
 );
 
+INSERT INTO users (firstname, lastname, username, password, role)
+VALUES
+("Gillian",  "Thompson",  "GillyT10", "$2a$10$kTJMa8NiWpcGW8GC.NtUy.q28VUCpO5H1/v9exYNr8BgUgN2yWi4q" , 'ADMIN');
